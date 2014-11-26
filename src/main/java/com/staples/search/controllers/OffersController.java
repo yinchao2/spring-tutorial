9833bbd71cc963682312ca15b7784095b898c5d2
Yin Chao <yin_chao@ymail.com>
2014-11-26 17:53:05 -0600

    add jstl

--- src/main/java/com/staples/search/controllers/OffersController.java
+++ src/main/java/com/staples/search/controllers/OffersController.java
@@ -1,16 +1,15 @@
 package com.staples.search.controllers;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OffersController {
 	
 	@RequestMapping("/")
-	public String showHome(HttpSession session) {
-		session.setAttribute("name", "John");
+	public String showHome(Model model) {
+		model.addAttribute("name", "<b>Jim</b>");
 		return "home";
 	}
 
