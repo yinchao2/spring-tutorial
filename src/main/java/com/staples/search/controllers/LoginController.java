05c921fe753ae56972c107d3e8c00abb51ccd8c3
YIN CHAO <yin_chao@ymail.com>
2014-12-04 01:11:34 -0600

    login controller and custom login page

---
+++ src/main/java/com/staples/search/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.staples.search.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+}
