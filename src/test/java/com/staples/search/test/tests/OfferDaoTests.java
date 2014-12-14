ab5dd08861b07d3a0a94d9c149858caef615dcec
YIN CHAO <yin_chao@ymail.com>
2014-12-14 03:55:05 -0600

    set up datasource config file for junit and create junit test class for OfferDao and UserDao

---
+++ src/test/java/com/staples/search/test/tests/OfferDaoTests.java
@@ -0,0 +1,77 @@
+package com.staples.search.test.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.staples.search.dao.Offer;
+import com.staples.search.dao.OffersDao;
+
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations = {
+		"classpath:com/staples/search/config/dao-context.xml",
+		"classpath:com/staples/search/config/security-context.xml",
+		"classpath:com/staples/search/test/config/datasource.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class OfferDaoTests {
+	
+	@Autowired
+	private OffersDao offersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+		
+		jdbc.execute("delete from offers");
+		jdbc.execute("delete from users");
+		jdbc.execute("delete from authorities");
+	}
+	
+	@Test
+	public void testCreateUser() {
+
+		Offer offer = new Offer("johnwpurcell", "john@caveofprogramming.com", "This is a test offer.");
+		
+		assertTrue("Offer creation should return true", offersDao.create(offer));
+		
+		List<Offer> offers = offersDao.getOffers();
+		
+		assertEquals("Should be one offer in database.", 1, offers.size());
+		
+		assertEquals("Retrieved offer should match created offer.", offer, offers.get(0));
+		/*
+		// Get the offer with ID filled in.
+		offer = offers.get(0);
+		
+		offer.setText("Updated offer text.");
+		assertTrue("Offer update should return true", offersDao.update(offer));
+		
+		Offer updated = offersDao.getOffer(offer.getId());
+		
+		assertEquals("Updated offer should match retrieved updated offer", offer, updated);
+		
+		offersDao.delete(offer.getId());
+		
+		List<Offer> empty = offersDao.getOffers();
+		
+		assertEquals("Offers lists should be empty.", 0, empty.size());
+		*/
+	}
+	
+}
---
+++ src/test/java/com/staples/search/test/tests/UserDaoTests.java
@@ -0,0 +1,60 @@
+package com.staples.search.test.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.staples.search.dao.User;
+import com.staples.search.dao.UsersDao;
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations = {
+		"classpath:com/staples/search/config/dao-context.xml",
+		"classpath:com/staples/search/config/security-context.xml",
+		"classpath:com/staples/search/test/config/datasource.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTests {
+	
+	@Autowired
+	private UsersDao usersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@Before
+	public void init() {
+		JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+		
+		jdbc.execute("delete from users");
+		jdbc.execute("delete from authorities");
+	}
+	
+	@Test
+	public void testCreateUser() {
+		User user = new User("johnwpurcell", "hellothere", "john@caveofprogramming.com", true, "ROLE_USER");
+		
+		assertTrue("User creation should return true", usersDao.create(user));
+		
+		List<User> users = usersDao.getAllUsers();
+		
+		assertEquals("Number of users should be 1.", 1, users.size());
+		
+		assertTrue("User should exist.", usersDao.exists(user.getUsername()));
+		
+		assertEquals("Created user should be identical to retrieved user", user, users.get(0));
+		
+	}
+	
+}
