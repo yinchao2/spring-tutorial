c1fdadb492ff729f464035a79eb347df4b622639
YIN CHAO <yin_chao@ymail.com>
2014-12-14 02:04:23 -0600

    add log4j.properties file

--- src/main/java/com/staples/search/controllers/HomeController.java
+++ src/main/java/com/staples/search/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.staples.search.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	private static Logger logger = Logger.getLogger(HomeController.class);
+
 	@RequestMapping("/")
 	public String showHome() {
+		logger.debug("show debug home controller...");
 		return "home";
 	}
-	
+
 }
